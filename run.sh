256f66166b24605cd1f7c14e3e0d89d2eb73a63a
gustavo-b <gustavo.hsbatista@gmail.com>
2019-11-20 22:48:33 -0300

    Adding PROJECT_ORGANIZATION to container arguments

--- run.sh
+++ run.sh
@@ -2,13 +2,14 @@
 
 set -x
 
+PROJECT_ORGANIZATION="${PROJECT_ORGANIZATION:-default}"
 PROJECT_KEY="${PROJECT_KEY:-ConsoleApplication1}"
 PROJECT_NAME="${PROJECT_NAME:-ConsoleApplication1}"
 PROJECT_VERSION="${PROJECT_VERSION:-1.0}"
 SONAR_HOST="${HOST:-http://localhost:9000}"
 SONAR_LOGIN_KEY="${LOGIN_KEY:-admin}"
 
-mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe begin /d:sonar.host.url=$SONAR_HOST /d:sonar.login=$SONAR_LOGIN_KEY /k:$PROJECT_KEY /n:"$PROJECT_NAME" /v:$PROJECT_VERSION
+mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe begin /d:sonar.host.url=$SONAR_HOST /d:sonar.login=$SONAR_LOGIN_KEY /k:$PROJECT_KEY /o:"$PROJECT_ORGANIZATION" /n:"$PROJECT_NAME" /v:$PROJECT_VERSION
 dotnet restore
 dotnet build
 dotnet test
